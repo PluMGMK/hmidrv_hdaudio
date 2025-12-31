ba4be994ba7a4ede35ef4301709cfa35067177cf
Michael Keyes <mgkeyes@vigovproductions.net>
2025-12-31 16:50:39 +0000

    Something something case-sensitive filesystem...

--- unixbld.sh
+++ unixbld.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-uasm -bin DRVSRC/hda16s.asm
-mv -v hd{a,w}16s.BIN
-uasm -bin -D?FLASHTEK=1 DRVSRC/hda16s.asm
-./hmiappnd oldfile=$HOME/Games/rayman-forever/Rayman/HMIDRV.OLD newfile=$HOME/Games/rayman-forever/Rayman/HMIDRV.386 drvf:E040=hda16s.BIN drvr:E040=hdw16s.BIN
+uasm -bin DRVSRC/HDA16S.ASM
+mv -v HD{A,W}16S.BIN
+uasm -bin -D?FLASHTEK=1 DRVSRC/HDA16S.asm
+./hmiappnd oldfile=$HOME/Games/rayman-forever/Rayman/HMIDRV.OLD newfile=$HOME/Games/rayman-forever/Rayman/HMIDRV.386 drvf:E040=HDA16S.BIN drvr:E040=HDW16S.BIN
 
 uasm -bin DRVSRC/HDA16SD.ASM
 mv -v HD{A,W}16SD.BIN
